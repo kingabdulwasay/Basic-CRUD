32944f1bec2fc59f160dd87215645bb4ed4d2b3a
kingabdulwasay <awasay245@gmail.com>
2026-07-19 08:37:59 -0700

    Stage 4: Full CRUD

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const tasks = [
     {id:2,name:"Task 2",done:true},
     {id:3,name:"Task 3",done:false},
 ]
+// app.use('/docs',)
 app.get('/', (req, res) => {
   res.status(200).json({ "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] });
 });
@@ -37,6 +38,33 @@ app.post('/tasks', (req, res) => {
     }else{
         tasks.push(task)
         res.status(201).send(task)
+    }
+})
+
+app.put('/task/:id', (req, res) => {
+    var newTask = req.body
+    var index = tasks.findIndex(t => t.id == req.params.id)
+    if(newTask.title === ""){
+        res.status(400).send({})
+
+    }else if(index === -1){
+        res.status(404).json({ "error": `Task ${req.params.id} not found`})
+
+    }else{
+        tasks[index] = newTask
+        res.status(201).send({"Updated": newTask })
+    }
+})
+
+app.delete('/task/:id', (req, res) => {
+    var id = req.params.id
+    var index = tasks.findIndex(t => t.id == id)
+    if(index === -1){
+        res.status(404).json({ "error": `Task ${id} not found` })
+
+    }else{
+        tasks.splice(index, 1)
+        res.status(204).send()
     }
 })
 
